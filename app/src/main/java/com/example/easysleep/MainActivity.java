18a70931125cdfea7e3c2cbaa206aef4560de532
ZDENEK KROUSKY - Student <G00267758@gmit.ie>
2020-04-04 16:27:17 +0100

    toggle BT using broadcastReceiver

--- app/src/main/java/com/example/easysleep/MainActivity.java
+++ app/src/main/java/com/example/easysleep/MainActivity.java
@@ -2,13 +2,86 @@ package com.example.easysleep;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.bluetooth.BluetoothAdapter;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = "Main Activity";
+
+    BluetoothAdapter btAdapter;
+
+    private final BroadcastReceiver broadcastReceiver1 = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if(action.equals(btAdapter.ACTION_STATE_CHANGED)) {
+                final int state = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, btAdapter.ERROR);
+
+                switch(state) {
+                    case BluetoothAdapter.STATE_OFF:
+                        Log.d(TAG, "broadcastReceiver1: STATE_OFF");
+                        break;
+                    case BluetoothAdapter.STATE_TURNING_OFF:
+                        Log.d(TAG, "broadcastReceiver1: STATE_TURNING_OFF");
+                        break;
+                    case BluetoothAdapter.STATE_TURNING_ON:
+                        Log.d(TAG, "broadcastReceiver1: STATE__TURNING_ON");
+                        break;
+                    case BluetoothAdapter.STATE_ON:
+                        Log.d(TAG, "broadcastReceiver1: STATE_ON");
+                        break;
+                }
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btOnOff = findViewById(R.id.toggleBTButton);
+
+        btAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        btOnOff.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toggleBT();
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.d(TAG, "onDestroy(): called");
+        super.onDestroy();
+        unregisterReceiver(broadcastReceiver1);
+    }
+
+    public void toggleBT() {
+        if(btAdapter == null) {
+            Log.d(TAG, "toggleBT : Your device doesn't support BT");
+        }
+        if(!btAdapter.isEnabled()) {
+            Intent enableBTIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivity(enableBTIntent);
+
+            IntentFilter btIntent = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
+            registerReceiver(broadcastReceiver1, btIntent);
+        }
+        if(btAdapter.isEnabled()) {
+            btAdapter.disable();
+
+            IntentFilter btIntent = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
+            registerReceiver(broadcastReceiver1, btIntent);
+        }
     }
 }
